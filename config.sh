e0857ecf1684b7cc8a7d100cd6bf2f549eb363cb
vnxrgb <53448733+vnxrgb@users.noreply.github.com>
2020-12-07 12:39:32 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -9,11 +9,11 @@ KBUILD_BUILD_HOST="enola"
 export oem=xiaomi 
 export device=pheonix
 
-ROM="PixelExperience"
-manifest_url="https://github.com/PixelExperience/manifest"
-export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+ROM="YAAP"
+manifest_url="https://github.com/yaap/manifest"
+export rom_vendor_name="yaap" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
 branch="pie"
 
-release_repo="Akianonymus/harpia_builds"
+release_repo="vnxrgb/vnx_builds"
 
 timezone="Asia/Kolkata"
